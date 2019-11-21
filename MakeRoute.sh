09b4a8fe64efbb227609e11e7772d0b851a8007b
SHOMA Shimahara <shoma@yk.rim.or.jp>
2019-11-22 07:27:46 +0900

    Refine

--- MakeRoute.sh
+++ MakeRoute.sh
@@ -9,9 +9,13 @@ sudo -k
 # Check $ROOTPASS
 [ ! $( echo $ROOTPASS |sudo -S whoami 2>/dev/null |grep root) ] && echo "root-passwd is wrong!" && exit 1
 
-echo $ROOTPASS |sudo -S route delete -net 192.168.100 2>/dev/null ;sudo -k
-
-echo $ROOTPASS |sudo -S route add -net 192.168.100 192.168.101.254 2>/dev/null ;sudo -k
 echo $ROOTPASS |sudo -S route add -host 192.168.100.1 192.168.101.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.100.2 192.168.101.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.100.3 192.168.101.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.100.4 192.168.101.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.101.1 192.168.100.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.101.2 192.168.100.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.101.3 192.168.100.254 2>/dev/null ;sudo -k
+echo $ROOTPASS |sudo -S route add -host 192.168.101.4 192.168.100.254 2>/dev/null ;sudo -k
 
 
