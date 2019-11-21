c2b700333993878ed438cc4ba91f35c27ab0e6d1
SHOMA Shimahara <shoma@yk.rim.or.jp>
2019-11-22 07:27:25 +0900

    Refine

--- AddIPaddress.sh
+++ AddIPaddress.sh
@@ -18,19 +18,24 @@ case $ANS in
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.2/24 alias 2>/dev/null ;sudo -k
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.3/24 alias 2>/dev/null ;sudo -k
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.4/24 alias 2>/dev/null ;sudo -k
-   ;;
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.1/24 alias 2>/dev/null ;sudo -k
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.2/24 alias 2>/dev/null ;sudo -k
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.3/24 alias 2>/dev/null ;sudo -k
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.4/24 alias 2>/dev/null ;sudo -k
+    ;;
   d )
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.1/24 -alias 2>/dev/null ;sudo -k
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.2/24 -alias 2>/dev/null ;sudo -k
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.3/24 -alias 2>/dev/null ;sudo -k
     echo $ROOTPASS | sudo -S ifconfig en7 inet 192.168.100.4/24 -alias 2>/dev/null ;sudo -k
-    ;;
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.1/24 -alias 2>/dev/null ;sudo -k
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.2/24 -alias 2>/dev/null ;sudo -k
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.3/24 -alias 2>/dev/null ;sudo -k
+    echo $ROOTPASS | sudo -S ifconfig en8 inet 192.168.101.4/24 -alias 2>/dev/null ;sudo -k
+     ;;
   *)
     echo "something wrong!"
     ;;
 esac
 
-echo '$ ifconfig en7'
-ifconfig en7
-read ANS
 
