f5f72d3f8009da8dae7c01596677c574125f3254
SHOMA Shimahara <shoma@yk.rim.or.jp>
2019-11-22 22:54:44 +0900

    Refine

--- MakeRoute.sh
+++ MakeRoute.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+printf "Add or Delete [a/d]: "
+read ANS
+
 printf "Enter root-password: "
 read -s ROOTPASS
 printf "\n"
@@ -9,13 +12,30 @@ sudo -k
 # Check $ROOTPASS
 [ ! $( echo $ROOTPASS |sudo -S whoami 2>/dev/null |grep root) ] && echo "root-passwd is wrong!" && exit 1
 
-echo $ROOTPASS |sudo -S route add -host 192.168.100.1 192.168.101.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.100.2 192.168.101.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.100.3 192.168.101.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.100.4 192.168.101.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.101.1 192.168.100.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.101.2 192.168.100.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.101.3 192.168.100.254 2>/dev/null ;sudo -k
-echo $ROOTPASS |sudo -S route add -host 192.168.101.4 192.168.100.254 2>/dev/null ;sudo -k
+case $ANS in
+  a )
+    echo $ROOTPASS |sudo -S route add -host 192.168.100.1 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.100.2 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.100.3 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.100.4 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.101.1 192.168.100.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.101.2 192.168.100.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.101.3 192.168.100.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route add -host 192.168.101.4 192.168.100.254 2>/dev/null ;sudo -k
+    ;;
+  d )
+    echo $ROOTPASS |sudo -S route delete -host 192.168.100.1 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.100.2 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.100.3 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.100.4 192.168.101.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.101.1 192.168.100.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.101.2 192.168.100.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.101.3 192.168.100.254 2>/dev/null ;sudo -k
+    echo $ROOTPASS |sudo -S route delete -host 192.168.101.4 192.168.100.254 2>/dev/null ;sudo -k
+    ;;
+  * )
+    echo "something wrong!"
+    ;;
+esac
 
 
